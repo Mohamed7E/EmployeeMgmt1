d467fee5cfbf80b33ce613537c1aa3198ead17e7
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-12-20 13:34:16 +0200

    create function Database

---
+++ EmployeeMgmt1/Function.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeeMgmt1
+{
+    class Function
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+        public Function()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\task\Task2_C#\Data Base\EmpDb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+        public DataTable GetData(String Query)
+        {
+            
+                dt = new DataTable();
+                sda = new SqlDataAdapter(Query, Con);
+                sda.Fill(dt);
+                return dt;
+            
+
+
+        }
+
+        public int SetData(string Query)
+        {
+            int Cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            Cnt = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return Cnt;
+        }
+
+    }
+}
